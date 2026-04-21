5cfc47fe20a921d6d20b0920250173e74c452fb3
stellalagat <stellalagat058@gmail.com>
2026-04-21 09:25:49 +0300

    phase 8

--- smarttask-ai/src/App.jsx
+++ smarttask-ai/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import { useState, useEffect } from "react";
 
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
